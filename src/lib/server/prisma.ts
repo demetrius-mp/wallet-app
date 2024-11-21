942398a2ea6f4da37bedc2a84b231debfb3a6c87
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-11-21 16:29:25 -0400

    add `exists` extension

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,3 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient().$extends({
+	model: {
+		$allModels: {
+			async exists<T>(this: T, where: Prisma.Args<T, 'findFirst'>['where']): Promise<boolean> {
+				// Get the current model at runtime
+				const context = Prisma.getExtensionContext(this);
+
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				const result = await (context as any).findFirst({ where });
+				return result !== null;
+			}
+		}
+	}
+});

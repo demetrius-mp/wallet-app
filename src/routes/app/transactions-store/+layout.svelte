e2004cfd9d3705bf138b1d5013f340b9c3fb703f
Demetrius Panovitch <demetrius.mp789@gmail.com>
2024-11-24 11:21:40 -0400

    remove progressive enhanced pages
    im gonna work full server side until mvp is reached, only then will i work with progressive enhancement

--- src/routes/app/transactions-store/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import TransactionsProvider from '$lib/providers/transactions-provider/transactions-provider.svelte';
-
-	let { children } = $props();
-</script>
-
-<TransactionsProvider>
-	{@render children()}
-</TransactionsProvider>

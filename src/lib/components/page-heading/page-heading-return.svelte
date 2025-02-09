b0205f8b0da3681238fd21d98ae487a7d3b3c050
Demetrius Panovitch <demetrius.mp789@gmail.com>
2025-02-09 08:37:54 -0400

    remove margin top for better alignment

--- src/lib/components/page-heading/page-heading-return.svelte
+++ src/lib/components/page-heading/page-heading-return.svelte
@@ -13,7 +13,7 @@
 <a {href} class="mb-2 flex cursor-pointer items-center text-muted-foreground hover:text-primary">
 	<ChevronLeftIcon class="mr-1.5 h-4 w-4 " />
 
-	<span class="mt-0.5 text-sm">
+	<span class="text-sm">
 		{@render children()}
 	</span>
 </a>
